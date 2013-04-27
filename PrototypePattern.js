69c4e399138ba1e2933e15160eee695503d5e6d1
rdecastro <zengineer@me.com>
2013-04-27 12:19:02 -0700

    Added initial prototype pattern

---
+++ PrototypePattern.js
@@ -0,0 +1,31 @@
+﻿// Prototype Pattern
+// Usage: 
+// 
+// var theInstance = new PrototypePattern();
+// theInstance.publicMethodA();
+
+var PrototypePattern = function () {
+
+    this.publicVariableA = 'A value';
+    this.publicVariableB = 'B value';
+
+};
+
+PrototypePattern.prototype = {
+
+    publicMethodA: function () {
+        var privateVariableA = 'hello world!';
+        return privateVariableA;
+    }
+
+  , publicMethodB: function (paramB) {
+        this.publicVariableB = paramB;
+        return this.publicVariableB;
+    }
+
+  , publicMethodC: function (paramC, paramD) {
+        console.log(paramC);
+        return paramD;
+  }
+
+}
